24f20d256825a46c548016bf931510fe9e483053
livetRaphael <raphael.livet@institutoptique.fr>
2025-04-23 16:46:22 +0200

    Projet 3 WPF : Logiciel de nettoyage

---
+++ SolutionCSharp/Projet3/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Projet3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Button_Click_Net(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Votre logiciel est nettoyé !");
+        }
+
+        private void Button_Click_MAJ(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Histo(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Web(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
