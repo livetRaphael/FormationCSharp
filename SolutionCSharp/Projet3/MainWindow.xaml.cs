6d62c2499dfd61b96f196faab85520570c278120
livetRaphael <raphael.livet@institutoptique.fr>
2025-04-24 13:19:57 +0200

    - Fonction d'analyse - Fonction de nettoyage

--- SolutionCSharp/Projet3/MainWindow.xaml.cs
+++ SolutionCSharp/Projet3/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,36 +22,134 @@ namespace Projet3
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DirectoryInfo winTemp;
+        public DirectoryInfo appTemp;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            winTemp = new DirectoryInfo(@"C:\Windows\Temp");
+            appTemp = new DirectoryInfo(System.IO.Path.GetTempPath());
         }
 
+        // Calcul de la taille d'un dossier
+        public long DirSize(DirectoryInfo dir)
+        {
+            return dir.GetFiles().Sum(fi => fi.Length) + dir.GetDirectories().Sum(di => DirSize(di));
+        }
 
+        // Vider un dossier
+        public void ClearTempData(DirectoryInfo di)
+        {
+            foreach (FileInfo file in di.GetFiles())
+            {
+                try
+                {
+                    file.Delete();
+                    Console.WriteLine(file.FullName);
+                }
+                catch(Exception ex)
+                {
+                    continue;
+                }
+            }
+
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                try
+                {
+                    dir.Delete(true);
+                    Console.WriteLine(dir.FullName);
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+            }
+
+        }
 
         private void Button_Click_Net(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Votre logiciel est nettoyé !");
+            Console.WriteLine("Nettoyage en cours...");
+            btnClean.Content = "NETTOYAGE EN COURS";
+
+            Clipboard.Clear();
+            try
+            {
+                ClearTempData(winTemp);
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Erreur : " + ex.Message);
+            }
+
+            try
+            {
+                ClearTempData(appTemp);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur : " + ex.Message);
+            }
+            btnClean.Content = "NETTOYAGE TERMINE";
+            titre.Content = "Nettoyage effectué !";
+            MessageBox.Show("Votre logiciel est nettoyé !", "Nettoyage", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         private void Button_Click_MAJ(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("Votre logiciel est à jour !", "Mise à jour", MessageBoxButton.OK, MessageBoxImage.Information);
 
         }
 
         private void Button_Click_Histo(object sender, RoutedEventArgs e)
         {
+            MessageBox.Show("TODO : Page de l'historique", "Historique", MessageBoxButton.OK, MessageBoxImage.Information);
 
         }
 
         private void Button_Click_Web(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                Process.Start(new ProcessStartInfo("http://anthony-cardinale.fr/pc-cleaner")
+                { 
+                    UseShellExecute = true 
+                });
+  
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Erreur : " + ex.Message);
+            }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Button_Click_Ana(object sender, RoutedEventArgs e)
         {
-
+            AnalyseFolders();
         }
+    
+        public void AnalyseFolders()
+        {
+            Console.WriteLine("Début de l'analyse...");
+            long totalSize = 0;
+
+            try
+            {
+                totalSize += DirSize(winTemp) / 1000000;
+                totalSize += DirSize(appTemp) / 1000000;
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Impossible d'analyser les dossiers :" + ex.Message);
+            }
+            
+
+            espace.Content = totalSize + " Mb";
+            titre.Content = "Analyse effectué !";
+            date.Content = DateTime.Today;
+        }
+    
+    
+    
     }
 }
